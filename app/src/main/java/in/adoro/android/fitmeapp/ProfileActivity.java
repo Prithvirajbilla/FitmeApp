b7d47c915758d2e142f73304af88dca0737c5f31
Shobhit-Agarwal <shobhit.agarwal@adoro.in>
2016-02-27 17:56:00 +0530

    Pie chart activity

--- app/src/main/java/in/adoro/android/fitmeapp/ProfileActivity.java
+++ app/src/main/java/in/adoro/android/fitmeapp/ProfileActivity.java
@@ -20,6 +20,9 @@ public class ProfileActivity extends BaseActivity {
     @Bind(R.id.random_text)
     public TextView txtRandom;
 
+    /*@Bind(R.id.fitChart)
+    public FitChart fitChart;*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,12 +36,21 @@ public class ProfileActivity extends BaseActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                fillFitChart();
             }
         });
 
-        final FitChart fitChart = (FitChart)findViewById(R.id.fitChart);
+        /*final FitChart fitChart = (FitChart)findViewById(R.id.fitChart);
         fitChart.setMinValue(0f);
         fitChart.setMaxValue(100f);
+        */
+
+
+    }
+
+    private void fillFitChart(){
+        final FitChart fitChart = (FitChart)findViewById(R.id.fitChart);
 
         Collection<FitChartValue> values = new ArrayList<>();
         values.add(new FitChartValue(30f, 0x2d4302));
@@ -46,7 +58,6 @@ public class ProfileActivity extends BaseActivity {
         values.add(new FitChartValue(15f, 0x8fc026));
         values.add(new FitChartValue(10f, 0xB5CC84));
         fitChart.setValues(values);
-
 
     }
 }
