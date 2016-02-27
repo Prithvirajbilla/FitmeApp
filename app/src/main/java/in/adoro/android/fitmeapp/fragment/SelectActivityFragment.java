da946e3cb96d9d5f2074186f91fa41ab0423cc3e
jkaka <jivan.molu@gmail.com>
2016-02-27 15:16:48 +0530

    changes

--- app/src/main/java/in/adoro/android/fitmeapp/fragment/SelectActivityFragment.java
+++ app/src/main/java/in/adoro/android/fitmeapp/fragment/SelectActivityFragment.java
@@ -1,26 +1,34 @@
 package in.adoro.android.fitmeapp.fragment;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import butterknife.Bind;
 
 import in.adoro.android.fitmeapp.R;
+import in.adoro.android.fitmeapp.adapter.SelectActivityAdapter;
 
-/**
- * Created by akshay on 2/27/16.
- */
+
 public class SelectActivityFragment extends Fragment {
 
     @Bind(R.id.recycler_view)
-    public RecyclerView mFeedRecyclerView;
+    public RecyclerView mRecyclerView;
 
-    /*private SelectActivityAdapter mAdapter;*/
+    private SelectActivityAdapter mAdapter;
     private GridLayoutManager mGridLayoutManager;
 
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_select_activity, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
     }
+
 }
